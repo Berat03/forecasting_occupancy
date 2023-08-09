e9bc94490f70bfedd41a4f9cc013f9673de92b19
berat03 <bbulbulwork@gmail.com>
2023-08-09 21:36:20 +0100

    new pre-processing function

--- query.py
+++ query.py
@@ -1,26 +1,25 @@
 import json
 import boto3
 from boto3.dynamodb.conditions import Key
-date2 = '2023-07-20'
-date = '2023-07-30'
-def lambda_handler(event, context):
+start = '2023-07-20'
+end = '2023-07-30'
+def lambda_handler(event= None, context= None):
     client = boto3.resource('dynamodb')
     table = client.Table('Bill_Bryson_Data')
 
-    response = table.get_item( # single
+    response = table.get_item(
         Key = {
-            'Date': date,
+            'Date': end,
             'Time': '00:00'
         }
     )
     print(response['Item'])
 
     response = table.query(
-        KeyConditionExpression = Key('Date').eq(date) & Key('Time').between('00:00', '24:00')
+        KeyConditionExpression =Key('Date').eq(end) & Key('Time').between('00:00', '24:00')
     )
 
     for item in response['Items']:
         print(item)
 
-
-lambda_handler(None, None)
+lambda_handler()
