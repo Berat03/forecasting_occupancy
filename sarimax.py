1646a14ca075c549978ccec79089d0f910b9f9a4
berat03 <bbulbulwork@gmail.com>
2023-08-09 22:37:46 +0100

    patchwork fix for errors from unimplemented exog df

--- sarimax.py
+++ sarimax.py
@@ -24,23 +24,23 @@ def find_parameters(time_series_data_col):
 
 
 # Apply ARIMA and print results
-def sarimax_apply(csv_data_file_path,resample_period, actual, pred_period, cut_off= 0):
+def sarimax_apply(csv_data_file_path, resample_period, actual, pred_period, cut_off=0):
     df = pre_process(csv_data_file_path=csv_data_file_path, resample_period=resample_period)
     df = df.iloc[:(len(df) - cut_off)]  # Getting rid of the mainly useless (empty)  holiday data
 
     # using result from auto.arima
-    mod = SARIMAX(endog=df,
+    mod = SARIMAX(endog=df['Total'],
                   order=(2, 0, 0),
                   trend='c')
     results_manual_sarima = mod.fit()
 
-    plt.plot(df[-actual:], label='Actual')
+    plt.plot(df['Total'][-actual:], label='Actual')
     predicted = results_manual_sarima.get_prediction(start=-(pred_period))  # Predictions for the last 100 time units
     plt.plot(predicted.predicted_mean, label='Predicted', linestyle='dashed')
     plt.legend()
     plt.show()
 
-    actual_values = df[-pred_period:].dropna()
+    actual_values = df['Total'][-pred_period:].dropna()
     predicted_mean = predicted.predicted_mean.dropna()
 
     # Calculate Mean Squared Error (MSE)
@@ -48,8 +48,7 @@ def sarimax_apply(csv_data_file_path,resample_period, actual, pred_period, cut_o
     print("Mean Squared Error (MSE):", mse)
 
 
-
 sarimax_apply(csv_data_file_path='./Data/Bill_Bryson_Data.csv',
-              resample_period= '30T', actual=300, pred_period=200, cut_off=100)
+              resample_period='30T', actual=300, pred_period=200, cut_off=100)
 sarimax_apply(csv_data_file_path='./Data/bbtable_data.csv',
-              resample_period= 'H', actual=200, pred_period=150, cut_off=400)
+              resample_period='H', actual=200, pred_period=150, cut_off=400)
