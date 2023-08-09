37f3ef34ae0aee084bc6756594b1967027add394
berat03 <bbulbulwork@gmail.com>
2023-08-09 21:48:59 +0100

    cleaned sarimax function

--- sarimax.py
+++ sarimax.py
@@ -3,7 +3,7 @@ import pmdarima as pmd
 import matplotlib.pyplot as plt
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from sklearn.metrics import mean_squared_error
-from data_preprocessing import pre_process
+from data_query_and_preprocessing import pre_process
 
 
 # ARIMA model fitting
@@ -24,7 +24,10 @@ def find_parameters(time_series_data_col):
 
 
 # Apply ARIMA and print results
-def sarimax_apply(df, actual, pred_period):
+def sarimax_apply(csv_data_file_path,resample_period, actual, pred_period, cut_off= 0):
+    df = pre_process(csv_data_file_path=csv_data_file_path, resample_period=resample_period)
+    df = df.iloc[:(len(df) - cut_off)]  # Getting rid of the mainly useless (empty)  holiday data
+
     # using result from auto.arima
     mod = SARIMAX(endog=df,
                   order=(2, 0, 0),
@@ -45,13 +48,8 @@ def sarimax_apply(df, actual, pred_period):
     print("Mean Squared Error (MSE):", mse)
 
 
-df_resampled = pre_process(csv_data_file_path="./Data/bbtable_data.csv", resample_period='H')
 
-df_resampled = df_resampled.iloc[:(len(df_resampled) - 450)] # Getting rid of the mainly useless (empty)  holiday data
-
-
-sarimax_apply(df_resampled, 200, 150)
-
-df_resampled = pre_process(csv_data_file_path="./Data/Bill_Bryson_Data.csv", resample_period='30T')
-
-sarimax_apply(df_resampled, 300, 200)
+sarimax_apply(csv_data_file_path='./Data/Bill_Bryson_Data.csv',
+              resample_period= '30T', actual=300, pred_period=200, cut_off=100)
+sarimax_apply(csv_data_file_path='./Data/bbtable_data.csv',
+              resample_period= 'H', actual=200, pred_period=150, cut_off=400)
