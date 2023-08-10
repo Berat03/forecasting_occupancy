b3b19af219808506b5ae0499453c470abc3aa6c7
berat03 <bbulbulwork@gmail.com>
2023-08-10 10:06:01 +0100

    exploring (weak) stationary, pacf plot, choosing parameters

---
+++ data_explore_and_process.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller, pacf, acf
+from statsmodels.graphics.tsaplots import plot_pacf, plot_acf
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def pre_process(csv_data_file_path, resample_period='H'):
+    df = pd.read_csv(csv_data_file_path)
+    df['Datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'])
+    df['Date'] = pd.to_datetime(df['Date'])
+    # Will need to create a loop, as we have multiple within year
+    # Exog
+    #holidays = pd.date_range(start='2023-07-14', end='2023-08-01', freq='D')
+    #terms = pd.date_range(start='2023-08-01', end='2023-08-07', freq='D')
+    #df['Holiday'] = df['Date'].isin(holidays)
+    #df['Term'] = df['Date'].isin(terms)
+    #df['Weekend'] = df['Datetime'].apply(lambda x: 1 if x.weekday() >= 5 else 0).astype(int)
+
+    df.set_index('Datetime', inplace=True)
+    df.sort_index(ascending=True)
+    df.drop(columns=['Date', 'Time'], inplace=True)
+
+    agg_functions = {'Total': 'median'} # , 'Weekend': 'max', 'Holiday': 'max', 'Term': 'max'
+    df_resample = df.resample(resample_period).agg(agg_functions).bfill()
+
+    """for i in ['Weekend', 'Holiday', 'Term']:
+        df_resample[i] = df_resample[i].astype(int)"""
+
+    assert (df_resample['Total'].isna().sum() == 0)
+    return df_resample
+
+
+df = pre_process(csv_data_file_path='./Data/bbtable_data.csv', resample_period='H')
+df = df.iloc[100:-1400]
+# Mean and variance changed based on indexing
+print(adfuller(df["Total"].values))
+# df['Total'] = df['Total'].diff()
+# df = df.iloc[1::] # due to differenceing
+# df.plot()
+# plt.show()
+
+plot_acf(df)
+plt.show()
+plot_pacf(df)
+plt.show()
+
+
+
+
