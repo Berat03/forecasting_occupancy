82ecf20aa7147e7e752835b7898daf1e99e66769
berat03 <bbulbulwork@gmail.com>
2023-08-10 10:05:04 +0100

    need to split into train-test

--- sarimax.py
+++ sarimax.py
@@ -3,7 +3,7 @@ import pmdarima as pmd
 import matplotlib.pyplot as plt
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from sklearn.metrics import mean_squared_error
-from data_query_and_preprocessing import pre_process
+from data_explore_and_process import pre_process
 
 
 # ARIMA model fitting
@@ -12,16 +12,15 @@ def find_parameters(time_series_data_col):
                                         start_p=0,
                                         start_d=0,
                                         start_q=0,
-                                        max_p=2,
-                                        max_d=2,
-                                        max_q=2,
+                                        max_p=3,
+                                        max_d=3,
+                                        max_q=3,
                                         trend='c',
                                         information_criterion='aic',
                                         trace=True,
                                         error_action='ignore')
 
     print(results_auto_arima)
-
 
 # Apply ARIMA and print results
 def sarimax_apply(csv_data_file_path, resample_period, actual, pred_period, cut_off=0):
@@ -30,7 +29,7 @@ def sarimax_apply(csv_data_file_path, resample_period, actual, pred_period, cut_
 
     # using result from auto.arima
     mod = SARIMAX(endog=df['Total'],
-                  order=(2, 0, 0),
+                  order=(3, 1, 0),
                   trend='c')
     results_manual_sarima = mod.fit()
 
@@ -48,7 +47,7 @@ def sarimax_apply(csv_data_file_path, resample_period, actual, pred_period, cut_
     print("Mean Squared Error (MSE):", mse)
 
 
-sarimax_apply(csv_data_file_path='./Data/Bill_Bryson_Data.csv',
-              resample_period='30T', actual=300, pred_period=200, cut_off=100)
+#sarimax_apply(csv_data_file_path='./Data/Bill_Bryson_Data.csv',
+             # resample_period='30T', actual=300, pred_period=200, cut_off=100)
 sarimax_apply(csv_data_file_path='./Data/bbtable_data.csv',
               resample_period='H', actual=200, pred_period=150, cut_off=400)
