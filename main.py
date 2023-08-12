45c0c6fc805defa60303addf7de122280853b116
berat03 <bbulbulwork@gmail.com>
2023-08-12 19:33:45 +0100

    running code

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from data_preprocess import pre_process
-import pandas as pd
-import matplotlib.pyplot as plt
-df = pre_process(csv_data_file_path='./Data/Bill_Bryson_Data.csv', resample_period='H')
-print(len(df) / 24)
-df = df.iloc[100:-200]
-df.plot()
-plt.show()
+from sarimax import pre_process, sarimax_apply, find_parameters
 
 
+df1 = pre_process(csv_data_file_path='./Data/Bill_Bryson_Data.csv', resample_period='H')
+sarimax_apply(df=df1, pred_period=300, forecast_periods=100, cut_off=100)
+
+#find_parameters(df=df1, col='Total', cut_off=0, exog_var='Weekend')
+
+
+"""Calculate Mean Squared Error (MSE)
+"""
