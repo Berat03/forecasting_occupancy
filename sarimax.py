77a1203ba89ad24273ca7853906899b23b142276
berat03 <bbulbulwork@gmail.com>
2023-08-12 15:55:23 +0100

    added seasonal parameters, much more accurate forecasting vs prediction

--- sarimax.py
+++ sarimax.py
@@ -5,7 +5,6 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 from sklearn.metrics import mean_squared_error
 from data_preprocess import pre_process
 
-
 """Calculate Mean Squared Error (MSE)
 actual_values = df['Total'][-pred_period:].dropna()
 predicted_mean = predicted.predicted_mean.dropna()
@@ -14,8 +13,9 @@ print("Mean Squared Error (MSE):", mse)"""
 
 
 # ARIMA model fitting
-def find_parameters(df, col, train=0.8): # Need to make so can put df in
-    time_series_data_col = df[col].iloc[:int(len(df) *train)]
+def find_parameters(df, col, train=0.8, cut_off=0):  # Need to make so can put df in
+    df = df.iloc[:(len(df) - cut_off)]  # Getting rid of the mainly useless (empty) holiday data
+    time_series_data_col = df[col].iloc[:int(len(df) * train)]
     results_auto_arima = pmd.auto_arima(time_series_data_col,
                                         start_p=0,
                                         start_d=0,
@@ -24,6 +24,8 @@ def find_parameters(df, col, train=0.8): # Need to make so can put df in
                                         max_d=10,
                                         max_q=10,
                                         trend='c',
+                                        seasonal=True,
+                                        m=24,
                                         # add sarima modelling
 
                                         information_criterion='aic',
@@ -32,36 +34,41 @@ def find_parameters(df, col, train=0.8): # Need to make so can put df in
 
     print(results_auto_arima)
 
+
 # Apply ARIMA and print results
-def sarimax_apply(df, actual, pred_period, forecast_periods, cut_off=0):
-    df = df.iloc[:(len(df) - cut_off)]  # Getting rid of the mainly useless (empty) holiday data
+import matplotlib.pyplot as plt
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 
-    # using result from auto.arima
+def sarimax_apply(df, pred_period, forecast_periods, cut_off=0, *args, **kwargs):
+    df = df.iloc[:(len(df) - cut_off)]
+
     mod = SARIMAX(endog=df['Total'],
-                  order=(7, 1, 4),
-                  trend='c', )
+                  order=(4, 1, 1),
+                  seasonal_order=(3, 0, 1, 24),
+                  trend='c')
 
-    # Fit the SARIMAX model
-    results = mod.fit()
+    results = mod.fit(maxiter=100)
 
-    # Get predictions for historical data and forecasted data
     predicted = results.get_prediction(start=-pred_period)
     forecast = results.get_forecast(steps=forecast_periods)
 
-    # Plot historical and forecasted values
-    plt.plot(df.index[-actual:], df['Total'][-actual:], label='Historical Actual')
+    plt.plot(df.index[-pred_period:], df['Total'][-pred_period:], label='Actual (Historical and Forecasted)')
     plt.plot(predicted.predicted_mean.index, predicted.predicted_mean, label='Historical Predicted', linestyle='dashed')
     plt.plot(forecast.predicted_mean.index, forecast.predicted_mean, label='Forecasted', linestyle='dotted')
+
     plt.legend()
     plt.title('Actual vs Predicted vs Forecasted Values')
     plt.xlabel('Time')
     plt.ylabel('Value')
     plt.show()
 
-
-#sarimax_apply(df = pre_process(csv_data_file_path='./Data/bbtable_data.csv',resample_period='H'), actual=300,
-              #pred_period=120, forecast_periods=100, cut_off=1300)
-#find_parameters(df = pre_process(csv_data_file_path='./Data/bbtable_data.csv', resample_period='H'), col= 'Total' )
+# Example usage
+# sarimax_apply(df, pred_period=30, forecast_periods=10, cut_off=0)
 
 
 
+df1 = pre_process(csv_data_file_path='./Data/Bill_Bryson_Data.csv', resample_period='H').iloc[:-300]
+
+sarimax_apply(df=df1, actual=72,
+              pred_period=72, forecast_periods=60, cut_off=72)
+#find_parameters(df=df1, col='Total', cut_off=72)
