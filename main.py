bea0e750322206c578abe8f7ea50e459ea472b27
berat03 <bbulbulwork@gmail.com>
2023-08-17 18:56:55 +0100

    experimenting with different cutoff for data, vastly different results

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from sarimax import pre_process
 import matplotlib.pyplot as plt
 from statsmodels.tsa.seasonal import seasonal_decompose
 from sarimax import find_parameters, sarimax_apply
-from statsmodels.tools.eval_measures import rmse
 
 
 df1 = pre_process(csv_data_file_path='./Data/Bill_Bryson_Data.csv', resample_period='H')
-#sarimax_apply(df=df1, pred_period=300, forecast_periods=100, cut_off=200, order=(1,0,0), seasonal_order=(1, 0, 0, 24))
+sarimax_apply(df=df1, pred_period=72, forecast_periods=72, cut_off=250, order=(1,0,0), seasonal_order=(1, 0, 0, 24))
 
-find_parameters(df=df1, col='Total', cut_off=200, exog_var='Weekend')
+#find_parameters(df=df1, col='Total', cut_off=200, exog_var='Weekend')
 
 
 def acf_plots(df, col='Total', m=24):
@@ -32,3 +31,5 @@ def seasonal_plot(df):
     fig.set_size_inches((16, 9))
     fig.tight_layout()
     plt.show()
+
+#rmse 8.24938973897547
