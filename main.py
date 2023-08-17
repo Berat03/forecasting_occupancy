22842fabed4cdafb7ef53bac863d9e60202ef6a0
berat03 <bbulbulwork@gmail.com>
2023-08-17 16:32:46 +0100

    refactor comment

--- main.py
+++ main.py
@@ -6,6 +6,3 @@ sarimax_apply(df=df1, pred_period=300, forecast_periods=100, cut_off=100)
 
 #find_parameters(df=df1, col='Total', cut_off=0, exog_var='Weekend')
 
-
-"""Calculate Mean Squared Error (MSE)
-"""
