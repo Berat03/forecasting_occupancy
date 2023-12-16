f433af525468c023ccf3ba071a098917931fbf4b
berat03 <bbulbulwork@gmail.com>
2023-12-16 15:53:10 +0100

    Rework date query for given dates. Created function that returns last x hours occupancy in df (soon to be json)

--- Data/data_query.py
+++ Data/data_query.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import json
-import datetime
+from datetime import datetime, timedelta
 import boto3
-from boto3.dynamodb.conditions import Key
+from boto3.dynamodb.conditions import Key, Attr
 import csv
+import pytz
 
+## Constants
 dynamodb = boto3.resource('dynamodb')
+table_name = 'Bill_Bryson_Data'
+table = dynamodb.Table(table_name)
 
+###
 
-def get_aws_data_to_csv(table_name, csv_file_path):
-    table = dynamodb.Table(table_name)
+def get_aws_data_to_csv(csv_file_path: str) -> None:
     response = table.scan()
-
     data = response['Items']
 
     while 'LastEvaluatedKey' in response: # pagination of sort
@@ -41,9 +44,7 @@ def get_aws_data_to_csv(table_name, csv_file_path):
     print(f'saved to {csv_file_path}')
 
 
-def query(start_date, end_date, start_time, end_time, table_name):  # Query function, to be used in website
-    table = dynamodb.Table(table_name)
-
+def query_specific_period(start_date, end_date, start_time, end_time):  # Query function, to be used in website
     response = table.get_item(
         Key={
             'Date': start_date,
@@ -59,4 +60,32 @@ def query(start_date, end_date, start_time, end_time, table_name):  # Query func
     for item in response['Items']:
         print(item)
 
-get_aws_data_to_csv('Bill_Bryson_Data', './Data/Data_AWS_141223')
+
+def get_last_x_time(hours: int = 24) -> pd.DataFrame:
+    curr_datetime = datetime.now().astimezone(pytz.timezone('Europe/London'))
+    start_datetime = curr_datetime - timedelta(hours=hours)
+    curr_date = curr_datetime.strftime('%Y-%m-%d')
+    end_date = start_datetime.strftime('%Y-%m-%d')
+
+    # Filter items based on time in-memory
+
+    response_end = table.query(
+        KeyConditionExpression=Key('Date').eq(end_date)
+    )
+    response_curr = table.query(
+        KeyConditionExpression=Key('Date').eq(curr_date)
+    )
+
+    response = {'Items': []}
+    response['Items'].extend(response_end['Items'])
+    response['Items'].extend(response_curr['Items'])
+
+    df = pd.DataFrame(response['Items'])
+    df['Datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'])
+    df.set_index('Datetime', inplace=True)
+    df.drop(['Date', 'Time'], axis=1, inplace=True)
+
+    return df
+
+get_last_x_time()
+
